6cf8c9db60bcef8726fc521aa4edc1c12691e0a7
Ariel0707 <arielsanttos999@gmail.com>
2025-09-12 08:22:37 -0300

    fff

---
+++ aula12/ex13/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.getElementById(`msg`)
+    var foto = document.getElementById(`img`)
+    var data = new Date()
+   // var hora = data.getHours()
+  var hora = 15
+    msg.innerHTML = `Agora são ${hora} horas.`
+}
+if (hora >= 0 && hora < 12 ) {
+    //BOM DIA 
+   foto.src = `/dia22.jpg`
+   }
+    
+else if (hora >= 12 && hora < 18){
+    //BOA TARDE
+    foto.src = `img/tarde.jpg`
+}else{
+    //BOA NOITE
+    foto.src = `noite.png`
+}
