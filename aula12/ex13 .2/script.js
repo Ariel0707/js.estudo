fdcd1ed9c787c28eee1fd63d0ef4db2c5661f06b
Ariel0707 <arielsanttos999@gmail.com>
2025-09-18 09:53:52 -0300

    aa

--- aula12/ex13 .2/script.js
+++ aula12/ex13 .2/script.js
@@ -3,10 +3,9 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var foto = window.document.getElementById('imagem')
 var data = new Date()
-//var hora = data.getHours()
-var hora = 22
-msg.innerHTML = ` Agora são ${hora} horas.` 
-}
+var hora = data.getHours()
+//var hora = 17
+msg.innerHTML = ` Agora são ${hora} horas.`
 
 if (hora>= 0 && hora < 12){
 
@@ -15,8 +14,11 @@ foto.src = 'dia22.jpg'
 } else if (hora >= 12 && hora < 18) {
 foto.src = 'tarde.jpg'
 
-document.body.style.background = 'red'}
+document.body.style.background = 'orange'}
 
 else {imagem.src = 'noite.png'
+document.body.style.background = 'black'
+}
 
 }
+
