83423b06dababba85083a6923905841376ce59e8
Ariel0707 <arielsanttos999@gmail.com>
2025-09-18 22:09:29 -0300

    aaa

--- aula12/ex14/script.js
+++ aula12/ex14/script.js
@@ -6,21 +6,47 @@ var res = document.querySelector(`div#res`)
 
 if (fano.value.length == 0 || Number(fano.value) > ano) {
     window.alert(`erro`)
-
 }else {
-    var fsex = document.getElementsByName(`redsex`)
-   var idade = ano - Number(fano.value)
-   var genero = ``
-   if (fsex[0].checked){
-    genero = `Homem`
-
-   } else if(fsex[1].checked){
-    genero = `Mulher`
-
- }
-   res.innerHTML = `Detectamos ${genero} com   ${idade} anos`
+    var fsex = document.getElementsByName(`radsex`)
+    var idade = ano - Number(fano.value)
+    var genero = ``
+    var img = document.createElement(`img`)
+    img.setAttribute(`id`,`foto`)
+    if (fsex[0].checked){
+        genero = `Homem`
+        if(idade >= 0 && idade < 12){
+            //Criança
+            img.setAttribute(`src`, `bhomem.jpg`)
+        }else if (idade < 21 ){
+            //Jovem
+             img.setAttribute(`src`, `jhomem.jpg`)
+        }else if (idade < 50 ){
+            //aduto
+             img.setAttribute(`src`, `vhomem.jpg`)
+        }else {
+            //idoso
+             img.setAttribute(`src`, `vhomem.jpg`)
+        }
+    }else if(fsex[1].checked){
+        genero = `Mulher`
+        if(idade >= 0 && idade < 12){
+            //Criança
+            img.setAttribute(`src`, `bmulher.jpg`)
+        }else if (idade < 21 ){
+            //Jovem
+            img.setAttribute(`src`, `jmulher.jpg`)
+        }else if (idade < 50 ){
+            //aduto
+            img.setAttribute(`src`, `vmulher.jpg`)
+        }else {
+            //idoso
+            img.setAttribute(`src`, `vmulher.jpg`)
+        }
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
 }
-
-
-
+   
+ 
+  
 }
