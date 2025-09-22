6a1aac1a35f165937f74d563d37e735cc906918b
Ariel0707 <arielsanttos999@gmail.com>
2025-09-22 07:56:58 -0300

    a

---
+++ aula13/platica.js
@@ -0,0 +1,23 @@
+var c = 1
+
+do{
+    console.log(`passo ${c}`)
+    c++
+ } while(c <= 50)
+
+
+//var c = 1
+//while(c <= 500){
+   // console.log(`passo ${c}`)
+  //  c++
+//}
+
+
+
+
+//console.log(`ola tudo bem`)
+//console.log(`ola tudo bem`)
+//console.log(`ola tudo bem`)
+//console.log(`ola tudo bem`)
+//console.log(`ola tudo bem`)
+//console.log(`ola tudo bem`)
