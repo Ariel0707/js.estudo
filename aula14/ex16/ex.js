6ba2106d0cea0b1e627ecf8dbe275510cfae642d
Ariel0707 <arielsanttos999@gmail.com>
2025-09-23 21:07:34 -0300

    aaa

--- aula14/ex16/ex.js
+++ aula14/ex16/ex.js
@@ -1,10 +1,25 @@
 function contar(){
-var txti = ``
-var txtf = ``
-var txtp = ``
+    var txti = document.getElementById('txti')
+    var txtf = document.getElementById('txtf')
+    var txtp = document.getElementById('txtp')
+    var rep = document.querySelector('div#rep')
 
-for( var txti = 1; txtf  <= 5;txti++){
+for( var txti = ``; txti  <= txtf;txti++){
     console.log(txti)
-}console.log(`fim...`)
+   /* var inicio = Number(txti.value)
+    var fim = Number(txtf.value)
+    var passo = Number(txtp.value)*/
 
-}
+}  if (txtp <= 0){
+        alert('passo invalido! considerando passo 1')
+        txtp = txti
+    }
+
+    /* for( var c = inicio; c <= fim; c += passo){
+        rep.innerHTML += `${c} `
+        console.log(c)
+
+    }
+}*/rep.innerHTML = `${txti}`}
+
+
--- aula14/platica.js
+++ aula14/platica.js
@@ -1,4 +1,16 @@
-console.log(`vai começar...`)
+/*console.log(`vai começar...`)
 for( var c = 1; c  <= 5;c++){
     console.log(c)
-}console.log(`fim...`)
+}console.log(`fim...`)*/
+
+
+var txti = `1`
+var txtf = `10`
+var txtp = ``
+var rep = ``
+
+
+for( var txti = 0; txti  <= txtf;txti++){
+    console.log(txti)
+    rep.innerHTML = `${txti}`
+}
