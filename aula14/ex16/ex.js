87ffb1675b7687608f6d26fddf1e66da2a02f9c5
Ariel0707 <arielsanttos999@gmail.com>
2025-09-24 08:48:16 -0300

    a

--- aula14/ex16/ex.js
+++ aula14/ex16/ex.js
@@ -1,3 +1,15 @@
 function Gerar_Tabuada(){
+    var numero = document.querySelector(`div#ext1`)
+    var resultado = document.querySelector^(`div#res`)
+    var num = Number(numero.value)
+
+    if (numero.value.length == 0 | num == 0 ){
+        window.alert(`[ERRO] Digite um número válido!`)
+        resultado.innerHTML = ``
+    }else{
+        resultado.innerHTML = ``
+    }
+
+
 
 }
