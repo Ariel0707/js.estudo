d3b18d580465d14be6505423b2740bcf13a26ddf
Ariel0707 <arielsanttos999@gmail.com>
2025-09-24 08:14:22 -0300

    a

--- aula14/ex16/ex.js
+++ aula14/ex16/ex.js
@@ -1,25 +1,3 @@
-function contar(){
-    var txti = document.getElementById('txti')
-    var txtf = document.getElementById('txtf')
-    var txtp = document.getElementById('txtp')
-    var rep = document.querySelector('div#rep')
+function Gerar_Tabuada(){
 
-for( var txti = ``; txti  <= txtf;txti++){
-    console.log(txti)
-   /* var inicio = Number(txti.value)
-    var fim = Number(txtf.value)
-    var passo = Number(txtp.value)*/
-
-}  if (txtp <= 0){
-        alert('passo invalido! considerando passo 1')
-        txtp = txti
-    }
-
-    /* for( var c = inicio; c <= fim; c += passo){
-        rep.innerHTML += `${c} `
-        console.log(c)
-
-    }
-}*/rep.innerHTML = `${txti}`}
-
-
+}
