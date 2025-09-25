caa0e26e0de089faad4c347e6f455657ecaca4fe
Ariel0707 <arielsanttos999@gmail.com>
2025-09-24 22:22:29 -0300

    A

--- aula14/ex16/ex.js
+++ aula14/ex16/ex.js
@@ -1,17 +1,18 @@
 function Gerar_Tabuada(){
-    var numero = document.querySelector(`div#ext1`)
-    var resultado = document.querySelector^(`div#res`)
-    var num = Number(numero.value)
+    var numero = document.querySelector('#text1');
+    var resultado = document.querySelector('div#res');
 
-    if (numero.value.length == 0 | num == 0 ){
-        window.alert(`[ERRO] Digite um número válido!`)
-        resultado.innerHTML = ``
-    }else{
-        fo(var C = numero; C <=10; C ++){
-        resultado.innerHTML = += '${C} x ${num} = ${C*num}<br'>
-        }
+    // Valida se o campo de número está vazio
+    if (numero.value.length == 0){
+        window.alert(`[ERRO] Por favor, digite um número!`);
+        return; // Interrompe a função se não houver número
     }
 
+    var num = Number(numero.value)
+    resultado.innerHTML = ''; // Limpa o resultado anterior
 
-
+    // Gera a tabuada de 1 a 10
+    for(let c = 1; c <= 10; c++) {
+        resultado.innerHTML += `${num} x ${c} = ${num*c} <br>`;
+    }
 }
