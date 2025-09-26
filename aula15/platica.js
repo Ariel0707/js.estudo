cc90fcd5e867b7e8fdc97ece878a53c493e4cd0f
Ariel0707 <arielsanttos999@gmail.com>
2025-09-26 08:47:55 -0300

    aa

---
+++ aula15/platica.js
@@ -0,0 +1,13 @@
+let num  = [5,8,4,6,7]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O valor tem ${num.length} posições
+`)
+console.log(`O primeiro valor é ${num[0]}
+`)
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log(`O valor não foi encontrado`)
+}else{
+    console.log(`O valor está na posição ${pos}`) }
