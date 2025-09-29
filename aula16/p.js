711a876c5e5662090f668b6e1711d8462c777d8f
Ariel0707 <arielsanttos999@gmail.com>
2025-09-29 08:38:39 -0300

    a

--- aula16/p.js
+++ aula16/p.js
@@ -1,1 +1,12 @@
-f
+function Adicionar() {
+    let adi = document.getElementById(`txtn`)
+    let mostra = document.getElementById(`submit`)
+    if (adi.value.length == 0) {
+        window.alert(`[ERRO] Por favor, digite um número!`);
+        return;
+    }else(adi.value.length >= 101);{
+        window.alert(`[ERRO] Valor em validor!`);
+        return;
+    }
+
+}
