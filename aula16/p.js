7710794a67d8b63b3767f81874159c37a7e943b9
Ariel0707 <arielsanttos999@gmail.com>
2025-09-30 08:53:21 -0300

    qqq

--- aula16/p.js
+++ aula16/p.js
@@ -1,12 +1,38 @@
 function Adicionar() {
     let adi = document.getElementById(`txtn`)
     let mostra = document.getElementById(`submit`)
+    let respota = document.getElementById(`res`)
+    
     if (adi.value.length == 0) {
         window.alert(`[ERRO] Por favor, digite um número!`);
         return;
-    }else(adi.value.length >= 101);{
+    } else(adi.value.length > 100);{
         window.alert(`[ERRO] Valor em validor!`);
-        return;
+       return;
+    } {
+        let num = Number(adi.value)
+        respota.innerHTML = '';
+        for(let num in adi){
+            let item = document.createElement(`option`)
+            item.text = `${num} `
+            respota.appendChild(item)
+
+        }
     }
+        
+    
+     let num = Number(adi.value)
+    respota.innerHTML = '';
+      for(let num in adi){
+        let item = document.createElement(`option`)
+        item.text = `${num} `
+        respota.appendChild(item)
+
+        }
+    
+
+ 
+
+    
 
 }
