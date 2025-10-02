cac0082adb988e1f15e38d80ec49eba87df1255c
Ariel0707 <arielsanttos999@gmail.com>
2025-10-02 08:32:39 -0300

    7

--- aula16/modelo/ex12.js
+++ aula16/modelo/ex12.js
@@ -1,6 +1,30 @@
 let num = document.querySelector(`input#frun`)
-let liste = document.querySelector(`select#flesta`)
+let liste = document.querySelector(`select#flista`)
+let res = document.querySelector(`div#res`)
+let valores = []
+
+function isNumero(n){
+    if(Number(n)>= 1 && Number(n) <= 100) {
+        return true
+
+    }else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+   }
 
 function Adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+       valores.push(Number(num.value))
 
+    }else{
+        window.alert(`Valor invalido ou ja encontrado na lsta`)
+    }
 }
