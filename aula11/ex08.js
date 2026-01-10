da6a9eabe492031274af2e8dd1688237ebf21c88
Ariel0707 <arielsanttos999@gmail.com>
2026-01-10 14:18:49 -0300

    A
    A

--- aula11/ex08.js
+++ aula11/ex08.js
@@ -1,5 +1,6 @@
-var vel = 100.1
+var vel = 150.1
 console.log(`A velocidade do seu veiculo e ${vel}km/h!`)
-if(vel > 60) {
+if(vel > 60) {//condicao simples
     console.log(`Voce uttrapasou a velovidade pemitida. MULTADO!`)
 }
+console.log(`Dirija sempre usando cinto de seguranca!`)
--- aula12/ex13/script.js
+++ aula12/ex13/script.js
@@ -1,20 +1,55 @@
 function carregar() {
+   
     var msg = document.getElementById(`msg`)
     var foto = document.getElementById(`img`)
     var data = new Date()
-   // var hora = data.getHours()
-  var hora = 15
+    
+    var res = document.getElementById('res') 
+    var semana = data.getDay();
+    
+    var hora = data.getHours()
+    //var hora = 19
     msg.innerHTML = `Agora são ${hora} horas.`
-}
+
 if (hora >= 0 && hora < 12 ) {
     //BOM DIA 
-   foto.src = `dia22.jpg`
-   }
-    
-else if (hora >= 12 && hora < 18){
+   foto.src = `dia.webp`
+  
+}else if (hora >= 12 && hora < 18){
     //BOA TARDE
     foto.src = `tarde.jpg`
-}else{
+    document.body.style.background = 'orange'}
+
+
+    else{foto.src = `noite.png`
+    document.body.style.background = 'black'
     //BOA NOITE
-    foto.src = `noite.png`
+   }
+   
+   
+   switch (semana) {
+    case 0:
+          res.innerHTML += "Domingo dia de procrastinar!🔥";
+        break;
+    case 1:
+        res.innerHTML += "Segunda-feira dia de trabalhar!👨‍💼";
+        break;
+    case 2:
+        res.innerHTML += "Terça-feira vamo que vamo!👨‍💻";
+        break;
+    case 3:
+        res.innerHTML += "Quarta-feira metade da semana!😶‍🌫️";
+        break;
+    case 4:
+        res.innerHTML += "Quinta-feira quase lá!😩";
+        break;
+    case 5:
+        res.innerHTML += "Sexta-feira hora de to,mar uma breja!🍻";
+        break;
+    case 6:
+        res.innerHTML += "Sábado dia de descanso!🛌🏻";
+        break;
 }
+}
+
+
